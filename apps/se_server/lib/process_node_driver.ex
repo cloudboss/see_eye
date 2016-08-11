a70fafec3232d2917260975187ab13d6992b5885
Joseph Wright <rjosephwright@gmail.com>
2016-08-10 20:29:39 -0700

    Make shell explicit

--- apps/se_server/lib/process_node_driver.ex
+++ apps/se_server/lib/process_node_driver.ex
@@ -7,7 +7,7 @@ defmodule ProcessNodeDriver do
     env = erlexecify_env(
       Map.merge(System.get_env, %{"SE_MASTER" => Atom.to_string(Node.self), "SE_WORKER_ID" => uuid})
     )
-    cmd = 'se_worker start'
+    cmd = ['/bin/sh', '-c', 'se_worker start']
     opts = [:monitor, {:env, env}]
 
     {:ok, _, _} = :exec.run_link(cmd, opts)
