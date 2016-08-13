3a7679a8446a77d079c1edb7da1ec22aa80e1fc6
Joseph Wright <rjosephwright@gmail.com>
2016-08-13 09:08:05 -0700

    Add metadata to log messages

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,6 @@ use Mix.Config
 import_config "../apps/*/config/config.exs"
 
 config :logger, :console,
+  metadata: [:module, :function, :line],
   format: "$date $time [$level] $metadata$message\n",
   level: :debug
