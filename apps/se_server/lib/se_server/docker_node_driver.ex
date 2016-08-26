b471fa89a21dd0ec0d2715ec0d000530672efcc8
Joseph Wright <rjosephwright@gmail.com>
2016-08-25 19:34:38 -0700

    Get SeServer.DockerNodeDriver working.
    Docker image fixes:
    Add libncurses dependency needed by Erlang runtime
    Add additional dependencies in se_server docker image
    
    DockerNodeDriver fixes:
    Add missing environment variable to docker command.
    Fix erlexec options. Modify it to be called synchronously, and no longer capture
    stdout, rather let it be returned from synchronous call.
    Add full path to docker command, since the shell is not being used, and so PATH
    will not be searched for docker command.

--- apps/se_server/lib/se_server/docker_node_driver.ex
+++ apps/se_server/lib/se_server/docker_node_driver.ex
@@ -5,17 +5,20 @@ defmodule SeServer.DockerNodeDriver do
 
   def spawn_node(uuid) do
     env = [to_char_list("SE_MASTER=#{Atom.to_string(Node.self)}"),
-           to_char_list("SE_WORKER_ID=#{uuid}")]
+           to_char_list("SE_WORKER_ID=#{uuid}"),
+           to_char_list("SE_KOOKY=xyz")]
     cmd = [
-      'docker', 'run', '-d',
+      '/usr/bin/docker', 'run', '-d',
       '-e', Enum.at(env, 0),
       '-e', Enum.at(env, 1),
+      '-e', Enum.at(env, 2),
       '--net', 'see_eye',
       'se_worker:latest', 'foreground'
     ]
     opts = [
+      :sync,
       {:env, env},
-      {:stdout, fn (_, _, out) -> Logger.info(out) end},
+      :stdout,
       :stderr
     ]
     case :exec.run_link(cmd, opts) do
